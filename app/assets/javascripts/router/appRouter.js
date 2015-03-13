a92df6c90af11d0a692f0133eb2e03cc66763a96
王雪梅 <wxm112@me.com>
2015-03-13 12:21:40 +1100

    the test button is working now

--- app/assets/javascripts/router/appRouter.js
+++ app/assets/javascripts/router/appRouter.js
@@ -5,28 +5,15 @@ var app = app || {};
 app.AppRouter = Backbone.Router.extend({
 
   routes: {
-    '':'index',
-    'posts/:id':'viewPost'
+    'flights/1':'viewBook'
   },
 
-  index: function(){
-    // if (today === "Sunday"){
-    //   var appView = new AppView({collection: blogPosts});
-    // }else{
-    //   var appView = new AppView({collection: funnyPosts});
-    // }
-
-    // console.log('routed to index');
-    var appView = new app.AppView({collection: app.blogPosts});
-    appView.render();
-  },
-
-  viewPost:function(id){
-    // console.log('viewing post', id);
-    var post = app.blogPosts.get(id);
-    console.log(post.toJSON());
-    var postView = new app.PostView({model: post});
-    postView.render();
+  viewBook:function(){
+    console.log('viewing book');
+    // var post = app.blogPosts.get(id);
+    // console.log(post.toJSON());
+    // var postView = new app.PostView({model: post});
+    // postView.render();
   }
 
 });
