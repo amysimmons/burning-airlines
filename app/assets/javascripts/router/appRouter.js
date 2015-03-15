bd2c0322896f54b3e5a65d06adc699bcbef9de0b
王雪梅 <wxm112@me.com>
2015-03-15 14:37:12 +1100

    seats view is working after fresh

--- app/assets/javascripts/router/appRouter.js
+++ app/assets/javascripts/router/appRouter.js
@@ -22,15 +22,15 @@ app.AppRouter = Backbone.Router.extend({
 
   viewPlane: function (id) {
     $('#main').empty();
-    console.log('viewPlane'); 
+    app.burningPlanes.fetch().done(function () {
       var plane = app.burningPlanes.get(id); 
       var planeView = new app.PlaneView({model: plane}); 
-      planeView.render();  
+      planeView.render(); 
+    });  
   },
 
   viewFlight: function (id) {
     $('#main').empty();
-    console.log('viewFlight');
     app.burningPlanes.fetch().done(function () {
       var flight = app.burningFlights.get(id); 
       var flightView = new app.FlightView({model: flight}); 
@@ -40,20 +40,17 @@ app.AppRouter = Backbone.Router.extend({
     }, 
 
   viewBook:function(id){
-    var flight = app.burningFlights.get(id); 
-    var plane_id = flight.attributes.plane_id; 
-    var options = {
-      flight: app.burningFlights.get(id), 
-      plane_id: flight.attributes.plane_id, 
-      plane: app.burningPlanes.get(plane_id) 
-
-    }
- 
-    console.log(options.flight.id);
-
-
-    var bookingView = new app.BookingView({model: options});
-    bookingView.render(options.plane);
+    app.burningFlights.fetch().done(function () {
+      var flight = app.burningFlights.get(id); 
+      var plane_id = flight.attributes.plane_id; 
+      var options = {
+        flight: app.burningFlights.get(id), 
+        plane_id: flight.attributes.plane_id, 
+        plane: app.burningPlanes.get(plane_id) 
+      }
+      var bookingView = new app.BookingView({model: options});
+      bookingView.render(options.plane);
+    }); 
   }, 
 
   viewSearch: function () {
